7c1e0788e1452a17f7fbbe56e46ac27e4dc1d285
Anton Abramov <ant.itdev@icloud.com>
2023-12-29 15:49:30 +0300

    Initial Commit

---
+++ FavoriteRegions/FavoriteRegionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteRegionsApp.swift
+//  FavoriteRegions
+//
+//  Created by Anton Abramov on 29.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteRegionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
